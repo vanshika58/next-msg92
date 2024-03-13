78fdc3a0e3db64f0e3b1138a79cef8154fc4ab3c
piyusssshh <piyush@whozzat.com>
2024-03-13 16:20:56 +0530

    PIYUSSSSSHH | PRICING | TEST

--- components/pricing/pricing-sms.js
+++ components/pricing/pricing-sms.js
@@ -18,98 +18,199 @@ const Pricingsms = ({
     countryCode,
 }) => {
     const [sliderValue, setSliderValue] = useState(47);
-    const [pricingEnv, setPricingEnv] = useState(4);
-    const [totalNoOfSmsArray, setTotalNoOfSmsArray] = useState([]);
+    const [noOfSection, setNoOfSection] = useState(0);
+
+    // const [pricingEnv, setPricingEnv] = useState(4);
+    // const [totalNoOfSmsArray, setTotalNoOfSmsArray] = useState([]);
+    // useEffect(() => {
+    //     setUtm();
+    // }, [pricing, originCountry, destinationCountry]);
+    // useEffect(() => {
+    //     if (process.env.PRICING_URL === 'https://test.msg91.com') {
+    //         setPricingEnv(4);
+    //     }
+    // }, []);
+
+    // useEffect(() => {
+    //     if (pricing.length > 0) {
+    //         setTotalNoOfSmsArray(pricing.map((item) => item[pricingEnv]['totalNoOfSms']).sort((a, b) => a - b));
+    //     }
+    // }, [pricing]);
+
+    // let noOfsms = 0,
+    //     pricingsms = 0,
+    //     ratePersms = 0;
+
+    // if (pricing[0] && pricing.length > 2) {
+    //     let arrayOfPrices = totalNoOfSmsArray.slice();
+    //     arrayOfPrices.unshift('0');
+
+    //     const lenAmountArr = totalNoOfSmsArray.length;
+    //     const widthOfSection = 100 / lenAmountArr;
+    //     setNoOfSection(Math.floor(sliderValue / widthOfSection));
+    //     // const noOfSection = 100/pricing.length
+    //     if (pricing[0]) {
+    //         if (pricing[noOfSection]) {
+    //             ratePersms = pricing[noOfSection][pricingEnv]?.rate;
+    //         } else {
+    //             ratePersms = pricing[noOfSection - 1][pricingEnv]?.rate;
+    //         }
+
+    //         const rangeInSection = lenAmountArr * (sliderValue - widthOfSection * noOfSection);
+    //         const noOfExtrasms = ((arrayOfPrices[noOfSection + 1] - arrayOfPrices[noOfSection]) * rangeInSection) / 100;
+    //         noOfsms = Number(arrayOfPrices[noOfSection]) + Math.floor(noOfExtrasms);
+    //     }
+    //     console.log(noOfSection, 77);
+    //     if (sliderValue == 100) {
+    //     }
+    //     let pricingSMSstr = pricing[noOfSection][pricingEnv]?.totalNoOfSms * pricing[noOfSection][pricingEnv]?.rate;
+    //     if (countryCode === 'IN') {
+    //         pricingsms = pricingSMSstr.toLocaleString('en-IN');
+    //     } else {
+    //         pricingsms = pricingSMSstr.toLocaleString(undefined);
+    //     }
+    //     noOfsms = Number(arrayOfPrices[noOfSection]);
+    // }
+    console.log('🚀 ~ pricing:', pricing.length);
     useEffect(() => {
-        setUtm();
-    }, [pricing, originCountry, destinationCountry]);
-    useEffect(() => {
-        if (process.env.PRICING_URL === 'https://test.msg91.com') {
-            setPricingEnv(4);
+        if (pricing[0] && pricing.length > 1) {
+            const totalSections = pricing.length;
+            const sectionSize = 100 / totalSections;
+            const currentSection = Math.floor(sliderValue / sectionSize);
+            setNoOfSection(currentSection);
+            console.log(currentSection);
         }
-    }, []);
+    }, [sliderValue]);
 
-    useEffect(() => {
-        if (pricing.length > 0) {
-            setTotalNoOfSmsArray(pricing.map((item) => item[pricingEnv]['totalNoOfSms']).sort((a, b) => a - b));
-        }
-    }, [pricing]);
-
-    let noOfsms = 0,
-        pricingsms = 0,
-        ratePersms = 0;
-
-    if (pricing[0] && pricing.length > 2) {
-        let arrayOfPrices = totalNoOfSmsArray.slice();
-        arrayOfPrices.unshift('-36');
-
-        const lenAmountArr = totalNoOfSmsArray.length;
-        const widthOfSection = 100 / lenAmountArr;
-        const noOfSection = Math.floor(sliderValue / widthOfSection);
-        if (pricing[0]) {
-            if (pricing[noOfSection]) {
-                ratePersms = pricing[noOfSection][pricingEnv]?.rate;
-            } else {
-                ratePersms = pricing[noOfSection - 1][pricingEnv]?.rate;
-            }
-
-            const rangeInSection = lenAmountArr * (sliderValue - widthOfSection * noOfSection);
-            const noOfExtrasms = ((arrayOfPrices[noOfSection + 1] - arrayOfPrices[noOfSection]) * rangeInSection) / 100;
-            noOfsms = Number(arrayOfPrices[noOfSection]) + Math.floor(noOfExtrasms);
-        }
-        if (sliderValue == 100) {
-            noOfsms = Number(arrayOfPrices[noOfSection]);
-        }
-        let pricingSMSstr = noOfsms * ratePersms;
-        if (countryCode === 'IN') {
-            pricingsms = pricingSMSstr.toLocaleString('en-IN');
-        } else {
-            pricingsms = pricingSMSstr.toLocaleString(undefined);
-        }
-    }
     return (
         <>
-            <div>
-                {originCountry?.length >= 1 && (
-                    <div className="d-flex flex-column flex-lg-row align-items-center  gap-4 ">
-                        <span className="Send-sms c-fw-m ">Send SMS from</span>
-                        <div className="gap-3 col d-flex flex-column text-start flex-md-row align-items-center justify-content-start col-12 col-md-10 col-lg-7">
-                            <Typeahead
-                                className="col c-fs-6"
-                                id="originCountry c-fs-6"
-                                placeholder="Origin Country"
-                                labelKey="name"
-                                onChange={(selected) => {
-                                    setPricing([]);
-                                    if (selected[0]?.name)
-                                        fetchSMSData(currency, selected[0]?.name, destinationCountry);
-                                }}
-                                options={countries}
-                                clearButton
-                                defaultSelected={[countries?.find((item) => item.name === originCountry)]}
-                                inputProps={{
-                                    autoComplete: 'off' /* Add the autoComplete attribute here */,
-                                }}
-                            />
+            {originCountry?.length >= 1 && (
+                <div className="d-flex flex-column flex-lg-row align-items-center  gap-4 ">
+                    <span className="Send-sms c-fw-m ">Send SMS from</span>
+                    <div className="gap-3 col d-flex flex-column text-start flex-md-row align-items-center justify-content-start col-12 col-md-10 col-lg-7">
+                        <Typeahead
+                            className="col c-fs-6"
+                            id="originCountry c-fs-6"
+                            placeholder="Origin Country"
+                            labelKey="name"
+                            onChange={(selected) => {
+                                setPricing([]);
+                                if (selected[0]?.name) fetchSMSData(currency, selected[0]?.name, destinationCountry);
+                            }}
+                            options={countries}
+                            clearButton
+                            defaultSelected={[countries?.find((item) => item.name === originCountry)]}
+                            inputProps={{
+                                autoComplete: 'off',
+                            }}
+                        />
 
-                            <div className="c-fw-m">To</div>
+                        <div className="c-fw-m">To</div>
 
-                            <Typeahead
-                                className="col"
-                                id="destinationCountry"
-                                placeholder="Destination Country"
-                                labelKey="name"
-                                onChange={(selected) => {
-                                    setPricing([]);
-                                    if (selected[0]?.name) fetchSMSData(currency, originCountry, selected[0]?.name);
-                                }}
-                                options={countries}
-                                clearButton
-                                defaultSelected={[countries?.find((item) => item.name === originCountry)]}
-                            />
-                        </div>
+                        <Typeahead
+                            className="col"
+                            id="destinationCountry"
+                            placeholder="Destination Country"
+                            labelKey="name"
+                            onChange={(e) => {
+                                const stepValue = 100 / pricing.length;
+                                let newValue = Number(e.target.value);
+                                // Ensure the slider does not go below the first step
+                                if (newValue < stepValue) {
+                                    newValue = stepValue;
+                                }
+                                // Adjust this condition to ensure the slider can reach the end
+                                if (newValue >= 100 - stepValue) {
+                                    newValue = 100;
+                                }
+                                setSliderValue(newValue);
+                            }}
+                            options={countries}
+                            clearButton
+                            defaultSelected={[countries?.find((item) => item.name === originCountry)]}
+                        />
                     </div>
-                )}
+                </div>
+            )}
+                  <input
+                className="slider"
+                type="range"
+                min="0"
+                max="100"
+                value={sliderValue}
+                step="14.2857142857" // Precise step value for 7 equal sections
+                onChange={(e) => {
+                    let newValue = parseFloat(e.target.value);
+                    const stepValue = 100 / 7;
+                    // Explicitly handle the edge case for the minimum value
+                    if (newValue <= stepValue / 2) {
+                        newValue = 0;
+                    } else if (newValue >= 100 - (stepValue / 2)) {
+                        // Explicitly handle the edge case for the maximum value
+                        newValue = 100;
+                    } else {
+                        // Calculate the index of the closest step (0 to 6)
+                        const stepIndex = Math.round(newValue / stepValue);
+                        // Set newValue to the exact value of the closest step
+                        newValue = stepIndex * stepValue;
+                    }
+                    setSliderValue(newValue);
+                }}
+                aria-label="Slider"
+            />
+            <div className="talk-to-sales connect-personalized mt-4">
+                <span className="personalized d-block c-fs-4">
+                    Connect with our team for a personalized pricing
+                    {originCountry === 'India' && (
+                        <>
+                            {' '}
+                            and get up to <span className="text-green c-fs-4 fw-medium"> ₹0.13</span> per SMS to meet
+                            your needs.{' '}
+                        </>
+                    )}
+                </span>
+                <button
+                    type="button"
+                    data-bs-toggle="modal"
+                    data-bs-target="#sales-modal"
+                    className="btn btn-outline-dark mt-2 mb-4 c-fs-5 border border-dark rounded-1 px-3 py-1"
+                >
+                    Talk to Sales
+                </button>
+                <br />
+                <a className="more-about" href="/sms">
+                    <img src="/img/icon/link.svg" alt="Know more" className="icon me-2" />
+                    <span>Know more about SMS</span>
+                </a>
+            </div>
+            {/* <div className="d-flex align-items-end mt-4 mb-3">
+                                    <p className="c-fs-2 c-fw-500">
+                                            <span className="c-fs-1 fw-bold">
+                                                {pricing[0][pricingEnv]?.totalNoOfSms}
+                                            </span>
+                                        SMS for{' '}
+                                        <span className="c-fs-1 text-green fw-bold">
+                                            {currencySymbol}
+                                            {pricingsms}{' '}
+                                        </span>{' '}
+                                        +18%GST at{' '}
+                                        <span className="c-fs-1 text-green fw-bold">
+                                            {currencySymbol}
+                                            {pricing[0][pricingEnv]?.rate}
+                                        </span>
+                                        per SMS{' '}
+                                    </p>
+                                </div> */}
+            <a
+                href="/signup?service=SMS"
+                target="_blank"
+                className={`btn btn-dark fw-semibold rounded-1 border border-2 border-dark px-3`}
+            >
+                Get Started
+            </a>
+            {/* <div>
+           
+           
                 {pricing[0] && (
                     <>
                         {pricing.length > 2 ? (
@@ -133,12 +234,22 @@ const Pricingsms = ({
                                     </div>
 
                                     <input
+                                        step={100 / totalNoOfSmsArray.length}
                                         className="slider"
                                         type="range"
                                         min="1"
                                         max="100"
-                                        value={sliderValue}
-                                        onChange={(e) => setSliderValue(e.target.value)}
+                                        value={
+                                            sliderValue < 100 / totalNoOfSmsArray.length
+                                                ? 100 / totalNoOfSmsArray.length
+                                                : sliderValue
+                                        }
+                                        onChange={(e) =>
+                                            setSliderValue(
+                                                Math.min(Math.max(100 / totalNoOfSmsArray.length, e.target.value), 100)
+                                            )
+                                        }
+          
                                         aria-label="Slider"
                                     />
 
@@ -154,29 +265,7 @@ const Pricingsms = ({
                                     </div>
                                     <div className="text-center text-dark c-fw-m">Cost per SMS</div>
                                 </div>
-                                <div className="d-flex align-items-end mt-4 mb-3">
-                                    <p className="c-fs-2 c-fw-500">
-                                        <span className="c-fs-1 fw-bold">{noOfsms.toLocaleString('en-IN')}</span> SMS
-                                        for{' '}
-                                        <span className="c-fs-1 text-green fw-bold">
-                                            {currencySymbol}
-                                            {pricingsms}{' '}
-                                        </span>{' '}
-                                        +18%GST at{' '}
-                                        <span className="c-fs-1 text-green fw-bold">
-                                            {currencySymbol}
-                                            {ratePersms}
-                                        </span>
-                                        per SMS{' '}
-                                    </p>
-                                </div>
-                                <a
-                                    href="/signup?service=SMS"
-                                    target="_blank"
-                                    className={`btn btn-dark fw-semibold rounded-1 border border-2 border-dark px-3`}
-                                >
-                                    Get Started
-                                </a>
+                             
                             </>
                         ) : (
                             <>
@@ -201,32 +290,8 @@ const Pricingsms = ({
                     </>
                 )}
 
-                <div className="talk-to-sales connect-personalized mt-4">
-                    <span className="personalized d-block c-fs-4">
-                        Connect with our team for a personalized pricing
-                        {originCountry === 'India' && (
-                            <>
-                                {' '}
-                                and get up to <span className="text-green c-fs-4 fw-medium"> ₹0.13</span> per SMS to
-                                meet your needs.{' '}
-                            </>
-                        )}
-                    </span>
-                    <button
-                        type="button"
-                        data-bs-toggle="modal"
-                        data-bs-target="#sales-modal"
-                        className="btn btn-outline-dark mt-2 mb-4 c-fs-5 border border-dark rounded-1 px-3 py-1"
-                    >
-                        Talk to Sales
-                    </button>
-                    <br />
-                    <a className="more-about" href="/sms">
-                        <img src="/img/icon/link.svg" alt="Know more" className="icon me-2" />
-                        <span>Know more about SMS</span>
-                    </a>
-                </div>
-            </div>
+          
+            </div> */}
         </>
     );
 };
